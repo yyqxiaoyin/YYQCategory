2a692fe0a2ee26f1c1d7f5df36f0725e0862daed
Mopon <357491060@qq.com>
2017-11-03 15:15:48 +0800

    版本升级维护

--- YYQCategory/Classes/UILabel/UILabel+YYQExtension.m
+++ YYQCategory/Classes/UILabel/UILabel+YYQExtension.m
@@ -8,8 +8,6 @@
 #import "UILabel+YYQExtension.h"
 #import <objc/runtime.h>
 
-static const void * kVerticalAlignmentKey = "kVerticalAlignmentKey";
-
 @implementation UILabel (YYQExtension)
 
 -  (CGFloat)attributeTextHeightWithSize:(CGSize)size
